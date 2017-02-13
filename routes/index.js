39a1f545b3b0708f92617a583dd2fe3cbf234fd6
WederPacheco <wederpacheco.s@gmail.com>
2017-02-13 00:59:12 -0200

    Preparando para enviar produtos dinamicamente

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const Produto = require('../models/produto');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Shopping Car' });
+  Produto.find(
+    (err,docs) => {
+      res.render('shop/index', {title: 'Meu e-commerce', produtos: docs});
+    }
+  )
 });
+
 
 module.exports = router;
--- seeds/seeder-produto.js
+++ seeds/seeder-produto.js
@@ -6,36 +6,38 @@ mongoose.connect('localhost:27017/shopping');
 let produtos = [
     new Produto({
         imagemPath: '../public/images/1.jpg',
-        title: 'Minha foto',
-        description: "Minha foto de batismo para venda",
+        titulo: 'Minha foto',
+        descricao: "Minha foto de batismo para venda",
         preco: 25
     }),
-    new Produto({
-        imagemPath: '../public/images/2.jpg',
-        title: 'Minha foto',
-        description: "Minha foto de batismo para venda",
-        preco: 25
-    }),
-    new Produto({
+     new Produto({
         imagemPath: '../public/images/3.jpg',
-        title: 'Minha foto',
-        description: "Minha foto de batismo para venda",
-        preco: 25
+        titulo: 'Minha foto',
+        descricao: "Minha foto de batismo para venda",
+        preco: 12
     }),
-    new Produto({
-        imagemPath: '../public/images/1.jpg',
-        title: 'Minha foto',
-        description: "Minha foto de batismo para venda",
-        preco: 25
+     new Produto({
+        imagemPath: '../public/images/4.jpg',
+        titulo: 'Minha foto',
+        descricao: "Minha foto de batismo para venda",
+        preco: 10
     }),
+     new Produto({
+        imagemPath: '../public/images/2.jpg',
+        titulo: 'Minha foto',
+        descricao: "Minha foto de batismo para venda",
+        preco: 30
+    })
 ];
 
 let counter = 0;
 
 for (let i =0; i < produtos.length; i++){
     produtos[i].save().then(
-        (err,result) => {
+        function(err,result){
             counter++;
+            if (err){console.log(err)}
+            console.log(result)
             if (counter === produtos.length){
                 mongoose.disconnect();
             }
